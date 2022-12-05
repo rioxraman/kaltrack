8c3b71727d5517f0502e2533751b56116d99b5ac
saibys <85431143+saibys@users.noreply.github.com>
2022-12-05 21:05:08 +0530

    button

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,10 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet} from 'react-native';
+
+
+import Button from '../components/UI/Button';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
 
 function ManageExpense({route,navigation}) {
    
@@ -10,7 +15,62 @@ function ManageExpense({route,navigation}) {
       title: isEditing ? 'Edit Expense' : 'Add Expense'
     })
   },[navigation,isEditing])
-  return <Text>ManageExpense Screen</Text>;
+
+  function deleteExpenseHandler(){
+    navigation.goBack();
+  }
+  function cancelHandler(){
+    navigation.goBack();
+  }
+  function confirmHandler(){
+    navigation.goBack();
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
