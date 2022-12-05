ba699a8f5acf40315cb6087cb3ee23ab68b0d54b
saibys <85431143+saibys@users.noreply.github.com>
2022-12-05 16:04:11 +0530

    added dummy data test

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, ImageBackground ,FlatList } from 'react-native';
+
+function renderExpenseItem(itemData) {
+    return <Text>{itemData.item.description}</Text>
+}
+
+function ExpensesList({expenses}) {
+    return(
+        <FlatList data={expenses} renderItem={renderExpenseItem} keyExtractor={(item)=>item.id} />
+        
+    )
+}
+export default ExpensesList;
