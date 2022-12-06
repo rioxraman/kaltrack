afe7c42b709a1db0fc4cb4415fb6c0765047e9e7
saibys <85431143+saibys@users.noreply.github.com>
2022-12-06 18:55:35 +0530

    input added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -3,7 +3,7 @@ import { View, StyleSheet} from 'react-native';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 
 
-import Button from '../components/UI/Button';
+
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import {ExpensesContext} from '../store/expenses-context'
